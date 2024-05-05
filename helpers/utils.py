f2dffebef6f45c5ce6cb44fd8d0662e7d3bf7cb7
Eric Minassian <eric@ericminassian.com>
2024-05-04 17:51:07 -0700

    scrape rotten tomatoes

--- helpers/utils.py
+++ helpers/utils.py
@@ -1,22 +1,116 @@
 import re
 from dataclasses import dataclass
-from typing import List, Optional
+from typing import List, Literal, Optional
 
+Category = Literal["movies_at_home", "movies_in_theaters", "tv_series_browse"]
+Sorting = Literal[
+    "sort:a_z",
+    "sort:popular",
+    "sort:newest",
+    "sort:critic_highest",
+    "sort:critic_lowest",
+    "sort:audience_highest",
+    "sort:audience_lowest",
+]
+Genre = Literal[
+    "action",
+    "adventure",
+    "animation",
+    "anime",
+    "biography",
+    "comedy",
+    "crime",
+    "documentary",
+    "drama",
+    "entertainment",
+    "faith_and_spirituality",
+    "fantasy",
+    "game_show",
+    "health_and_wellness",
+    "history",
+    "holiday",
+    "horror",
+    "house_and_garden",
+    "kids_and_family",
+    "lgbtq",
+    "music",
+    "musical",
+    "mystery_and_thriller",
+    "nature",
+    "news",
+    "reality",
+    "romance",
+    "sci_fi",
+    "short",
+    "soap",
+    "special_interest",
+    "sports",
+    "stand_up",
+    "talk_show",
+    "travel",
+    "variety",
+    "war",
+    "western",
+]
+Rating = Literal["g", "nc_17", "nr", "pg", "pg_13", "r", "ur"]
 
-def parse_currency_to_number(text: str) -> Optional[int]:
-    # Regular expression to match a currency value with an optional M/B suffix
-    pattern = r"[\$\€\£\¥]\s*(\d{1,3}(?:,\d{3})*(?:\.\d+)?)([MB]?)"
-    match = re.search(pattern, text)
-    if match:
-        amount, multiplier = match.groups()
-        number = float(amount.replace(",", ""))
-        # Adjust number based on the multiplier
-        if multiplier == "M":
-            number *= 1_000_000
-        elif multiplier == "B":
-            number *= 1_000_000_000
-        return int(number)
-    return None
+MAX_PAGES = 10
+BASE_URL = "https://www.rottentomatoes.com"
+CATEGORIES: List[Category] = [
+    "movies_at_home",
+    "movies_in_theaters",
+    "tv_series_browse",
+]
+SORTINGS: List[Sorting] = [
+    "sort:a_z",
+    "sort:popular",
+    "sort:newest",
+    "sort:critic_highest",
+    "sort:critic_lowest",
+    "sort:audience_highest",
+    "sort:audience_lowest",
+]
+GENRES: List[Genre] = [
+    "action",
+    "adventure",
+    "animation",
+    "anime",
+    "biography",
+    "comedy",
+    "crime",
+    "documentary",
+    "drama",
+    "entertainment",
+    "faith_and_spirituality",
+    "fantasy",
+    "game_show",
+    "health_and_wellness",
+    "history",
+    "holiday",
+    "horror",
+    "house_and_garden",
+    "kids_and_family",
+    "lgbtq",
+    "music",
+    "musical",
+    "mystery_and_thriller",
+    "nature",
+    "news",
+    "reality",
+    "romance",
+    "sci_fi",
+    "short",
+    "soap",
+    "special_interest",
+    "sports",
+    "stand_up",
+    "talk_show",
+    "travel",
+    "variety",
+    "war",
+    "western",
+]
+RATINGS: List[Rating] = ["g", "nc_17", "nr", "pg", "pg_13", "r", "ur"]
 
 
 @dataclass
@@ -49,3 +143,28 @@ class Media:
     release_date_streaming: Optional[str]
     runtime: Optional[str]
     box_office: Optional[int]
+
+
+def parse_currency_to_number(text: str) -> Optional[int]:
+    # Regular expression to match a currency value with an optional M/B suffix
+    pattern = r"[\$\€\£\¥]\s*(\d{1,3}(?:,\d{3})*(?:\.\d+)?)([MB]?)"
+    match = re.search(pattern, text)
+    if match:
+        amount, multiplier = match.groups()
+        number = float(amount.replace(",", ""))
+        # Adjust number based on the multiplier
+        if multiplier == "M":
+            number *= 1_000_000
+        elif multiplier == "B":
+            number *= 1_000_000_000
+        return int(number)
+    return None
+
+
+def create_url(
+    category: Category,
+    sorting: Sorting,
+    genre: Genre,
+    rating: Rating,
+) -> str:
+    return f"{BASE_URL}/browse/{category}/genres:{genre}~ratings:{rating}~{sorting}"
--- main.py
+++ main.py
@@ -1,14 +1,87 @@
-import requests
+import json
+from typing import List, Literal
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
 
 from helpers.media import parse_media_html
+from helpers.utils import (
+    BASE_URL,
+    CATEGORIES,
+    GENRES,
+    MAX_PAGES,
+    RATINGS,
+    SORTINGS,
+    Category,
+    Genre,
+    Rating,
+    Sorting,
+    create_url,
+)
+
+
+def get_media_urls():
+    options = webdriver.ChromeOptions()
+    # options.add_argument("--headless")
+    driver = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install()),
+        options=options,
+    )
+
+    def _get_media_urls(
+        category: Category, sorting: Sorting, genre: Genre, rating: Rating
+    ):
+        url = create_url(category, sorting, genre, rating)
+        driver.get(url)
+
+        print(url)
+
+        for i in range(1, MAX_PAGES):
+            with open(
+                f"output/{category}_{sorting}_{genre}_{rating}_{i}.html", "w"
+            ) as file:
+                file.write(driver.page_source)
+
+            try:
+                load_more_button = WebDriverWait(driver, 10).until(
+                    EC.element_to_be_clickable(
+                        (By.CSS_SELECTOR, 'button[data-qa="dlp-load-more-button"]')
+                    )
+                )
+                load_more_button.click()
+            except Exception:
+                break
+
+    for category in CATEGORIES:
+        for sorting in SORTINGS:
+            for genre in GENRES:
+                for rating in RATINGS:
+                    _get_media_urls(category, sorting, genre, rating)
+
+    driver.quit()
 
 
 def main() -> None:
-    url = "https://www.rottentomatoes.com/m/avengers_endgame"
-    response = requests.get(url)
+    get_media_urls()
 
-    if response.status_code == 200:
-        print(parse_media_html(response.text))
+    # with open("movies_at_home.html") as file:
+    #     html = file.read()
+
+    # soup = BeautifulSoup(html, "html.parser")
+
+    # for div in soup.find_all("div", {"class": "flex-container"}):
+    #     name = str(
+    #         div.find("span", {"data-qa": "discovery-media-list-item-title"}).text
+    #     ).strip()
+    #     relative_url = div.find("a")["href"]
+    #     url = f"{BASE_URL}{relative_url}"
+
+    #     print(name, url)
 
 
 if __name__ == "__main__":
